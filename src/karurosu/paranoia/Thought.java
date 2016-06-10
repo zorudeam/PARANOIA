ab7f47099dd410ca57e4abc0ac470bcdc3d5424c
Zorudeam <carlitosortegah@live.com>
2016-06-09 23:01:19 -0500

    Start of project, this is the core

--- src/karurosu/paranoia/Thought.java
+++ src/karurosu/paranoia/Thought.java
@@ -6,10 +6,10 @@ import java.io.Serializable;
  * Created by Zorudeam on 09/06/2016.
  */
 public class Thought implements Serializable{
-    String s;
+    String e;
 
     public Thought(String n, Type type, Intensity intensity){
-        this.s = n;
+        this.e = n;
     }
 
     enum Intensity { //0-8 LEVELS, 4 is static
